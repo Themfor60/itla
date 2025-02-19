acb8e7e71de0657e578010aa5b22b5d8b433e807
Uriel Aquino <143182625+Themfor60@users.noreply.github.com>
2025-02-19 09:22:26 -0400

    Update fecha de nacimiento.cs

--- fecha de nacimiento.cs
+++ fecha de nacimiento.cs
@@ -4,11 +4,11 @@ class Programa
 {
     static void Main()
     {
-        // Solicitar al usuario su fecha de nacimiento
+        
         Console.Write("Ingresa tu fecha de nacimiento (dd/mm/yyyy): ");
         DateTime fecha = Convert.ToDateTime(Console.ReadLine());
 
-        // Mostrar el día de la semana
+      
         Console.WriteLine("El día de la semana fue: " + fecha.DayOfWeek);
     }
 }
