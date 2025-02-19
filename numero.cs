d20c2883099af51cb31798fb21425224f4a11afd
Uriel Aquino <143182625+Themfor60@users.noreply.github.com>
2025-02-19 09:17:55 -0400

    Add files via upload

---
+++ numero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        int[] numeros = new int[10];
+
+        // Ingreso de 10 números
+        Console.WriteLine("Ingresa 10 números:");
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+            numeros[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        // Ordenar el arreglo
+        Array.Sort(numeros);
+
+        // Mostrar los números ordenados
+        Console.WriteLine("\nNúmeros ordenados:");
+        foreach (int numero in numeros)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
