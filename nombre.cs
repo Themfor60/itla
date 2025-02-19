712e74dc4b840cc6fd1aff71c6f794c3d0d6156b
Uriel Aquino <143182625+Themfor60@users.noreply.github.com>
2025-02-19 09:27:45 -0400

    Add files via upload

---
+++ nombre.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        // Preguntar al usuario su nombre
+        Console.Write("¿Cuál es tu nombre? ");
+        string nombre = Console.ReadLine();
+
+       
+        char inicial = nombre[0];
+
+      
+        Console.WriteLine($"La inicial de tu nombre es: {inicial}");
+    }
+}
